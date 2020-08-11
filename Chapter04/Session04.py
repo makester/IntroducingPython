e30666197455c07d31e140b499140869b16da6a8
Makester <makester.japan@gmail.com>
2020-08-12 08:32:55 +0900

    create first while

---
+++ Chapter04/Session04.py
@@ -0,0 +1,9 @@
+#4.3 if,elif,elseによる比較はAnkiに記入
+# コードなし、比較演算子とブール演算子についてまとめた
+
+#4.4 whileによる反復処理
+while True:
+    stuff = input("string to capitalize [type q to quit]: ")
+    if stuff == "q":
+        break
+    print(stuff.capitalize())
