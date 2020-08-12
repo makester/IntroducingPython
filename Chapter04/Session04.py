b52c77e0532a6fc06fe228bee2b541c2d91a9241
Makester <makester.japan@gmail.com>
2020-08-12 09:26:13 +0900

    add 4.5 code until 4.5.3

--- Chapter04/Session04.py
+++ Chapter04/Session04.py
@@ -32,3 +32,31 @@ while position < len(numbers):
     position += 1
 else: #breakが呼び出されていない
     print("偶数は見つかりませんでした")
+
+#4.5 forによる反復処理
+#辞書系のデータからキーと値の組をfor文を使ってタプルで返す
+
+#辞書の定義
+accusation = {
+    'room': 'ballroom',
+    'weapon': 'lead pipe',
+    'person': 'Col. Mustrad'
+}
+#タプルでキーとバリューと取り出す
+for item in accusation.items():
+    print(item)
+
+#4.5.3 elseによるbreakのチェック
+#forループが途中でbreakを呼ばずに最後まで実行されたことを確認するのに使える
+cheeses = []
+for cheese in cheeses:
+    print('This shop has some lovely', cheese)
+    break
+else: #breakしてないから"cheese"はない
+    print('This is not much of a cheese shop, is it?')
+
+#4.5.4 zip()を使った複数シーケンスの反復処理
+#dict()は2要素のシーケンスから辞書を作れるので、zip()で2要素のシーケンスを生成し
+#それをdict()に渡すと辞書が作れる
+
+dict(zip(range(1,10,1),range(2,20,2)))
