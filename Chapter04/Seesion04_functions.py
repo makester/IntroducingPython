8d29bdcb53f9154e09ab29eca84868fa56b6177a
Makester <makester.japan@gmail.com>
2020-08-14 09:08:48 +0900

    内包表記のレッスン終了

--- Chapter04/Seesion04_functions.py
+++ Chapter04/Seesion04_functions.py
@@ -29,3 +29,19 @@ for cell in cells:
 word = 'letter'
 letter_count = {text: word.count(text) for text in word}
 letter_count
+
+#4.6.3集合内包表記
+a_set = {number for number in range(1,6) if number % 3 == 1}
+a_set
+
+#4.6.4ジェネレータ内包表記
+number_things = (number for number in range(1,6))
+type(number_things)
+
+#ジェネレータを使ってみる
+for number in number_things:
+    print(number)
+
+#ジェネレータが一度しか使えないことの確認
+number_list = list(number_things)
+number_list
