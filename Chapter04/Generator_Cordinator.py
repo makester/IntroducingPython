9c8dae8870803535a8b5c43a0b4dfb91c10985b2
Makester <makester.japan@gmail.com>
2020-08-21 06:30:16 +0900

    add generator codeでやるとsyntax errorでる

--- Chapter04/Generator_Cordinator.py
+++ Chapter04/Generator_Cordinator.py
@@ -7,3 +7,38 @@ def my_range(first=0, last=10, step=1):
         yield number
         number += step
 
+#4.9 デコレータ
+#入力として関数をひとつ取り、別の関数を返すのがデコレータ
+
+#デコレータdocument_itの宣言
+def document_it(func):
+    def new_function(*args, **kwargs):
+        print('Running function:', func.__name__)
+        print('Positional arguments:', args)
+        print('keyword arguments:', kwargs)
+        result = func(*args, **kwargs)
+        print('Result:', result)
+        return result
+    return new_function
+
+#手作業でデコレータを使ってみる
+# def add_ints(a, b):
+#     return a + b
+
+# add_ints(3, 5)
+
+# cooler_add_ints = document_it(add_ints) #手作業でデコレータの戻り値を代入
+# cooler_add_ints(3, 5)
+
+#関数に対するデコレータは複数持てる。結果を自乗するデコレータを宣言する
+def square_it(func):
+    def new_function(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result * result
+    return new_function
+
+@square_it
+def add_ints(a, b):
+    return a + b
+
+
