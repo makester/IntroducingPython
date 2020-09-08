5801923d506999a49c5307b6bf4db4ab6668870f
Makester <makester.japan@gmail.com>
2020-09-09 08:13:55 +0900

    create module

--- Chapter05/weatherman/weatherman.py
+++ Chapter05/weatherman/weatherman.py
@@ -1,4 +1,4 @@
-import report
+import report#普通にインポート
 
-description = report.get_description()
+description = report.get_description()#モジュール全体を呼び出した時はモジュールプレフィック後に関数名
 print("Today's weather:", description)
