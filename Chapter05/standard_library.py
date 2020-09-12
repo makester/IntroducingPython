77dd981fba87791e15b9174f5e702a565d316cd9
Makester <makester.japan@gmail.com>
2020-09-12 19:58:39 +0900

    done standard library

--- Chapter05/standard_library.py
+++ Chapter05/standard_library.py
@@ -68,3 +68,93 @@ breakfast_counter - lunch_counter #朝食にあって昼食にない
 lunch_counter - breakfast_counter #昼食にあって朝食にない
 breakfast_counter & lunch_counter #両方にある
 breakfast_counter | lunch_counter #全部組み合わせると..
+
+#5.5.3 OrderdDict()によるキー順のソート
+quotes = {
+    'Moe': 'A wise guy, huh?',
+    'Curly': 'Nyuk nyuk!',
+    'Larry': 'Ow!',
+
+}
+for stooge in quotes:
+    print(stooge)
+#環境依存なのかこのコードはkeyを順番どおりに返してくる
+
+#キーが追加された順番を覚えておいて追加された順序にキーを返す
+#イテレータになれる辞書は"OrderDict()"で作成できる
+#ここではタプルのリストから辞書を作成している
+
+from collections import OrderedDict
+quotes = OrderedDict([('Moe', 'Awaise guy, huh?'),
+                      ('Larry', 'Ow!'),
+                      ('Curry', 'Nyku nyuk!'),
+                      ])
+for stooge in quotes:
+    print(stooge)
+
+#5.5.4 スタック+キュー=デック
+#両端から要素を追加削除できるキューとスタックの要素を組みあせたのがデック
+#このコードは単語の両端から文字を処理して回文かどうかをチェックするコード
+def palindrome(word):
+    from collections import deque
+    dq = deque(word)
+    while len(dq) > 1:
+        if dq.popleft() != dq.pop():
+            return False
+        return True
+
+palindrome('a')
+palindrome('racecar')
+palindrome('')
+palindrome('radar')
+palindrome('halibut')
+
+#スライスを使った効率的な回文チェッカー
+def another_palindrome(word):
+    return word == word[::-1]
+
+another_palindrome('a')
+another_palindrome('racecar')
+another_palindrome('')
+another_palindrome('radar')
+another_palindrome('halibut')
+
+#5.5.5 itertoolsによるコード構造の反復処理
+#itertoolsには特別な反復処理の関数が使える
+
+#itertools.chain
+#引数全体をひとつのイテラブルのように扱う
+import itertools
+for item in itertools.chain([1,2], ['a', 'b']):
+    print(item)
+
+#itertools.cycle
+#引数から循環的に要素を取り出す無限反復子
+import itertools
+for item in itertools.cycle(['a', 'b', 'c']):
+    print(item)
+
+#itertools.accumulate
+#要素間を足したり掛けたりしつつ値を返す
+import itertools
+for item in itertools.accumulate([1, 2, 3, 4]):
+    print(item)
+
+#accumulateは第二引数で2数値間の算出方法を指定できる
+import itertools
+def multiply(a, b):
+    return a * b
+
+for item in itertools.accumulate([1, 2, 3, 4], multiply):
+    print(item)
+
+#5.5.6 pprint()によるきれいな表示
+
+from collections import OrderedDict
+from pprint import pprint
+quotes = OrderedDict([('Moe', 'Awaise guy, huh?'),
+                      ('Larry', 'Ow!'),
+                      ('Curry', 'Nyku nyuk!'),
+                      ])
+print(quotes)
+pprint(quotes)
